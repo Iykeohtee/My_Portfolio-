7381fbe56ef482fa10c98a09e1bb39055e1c5f03
Ikechukwu <otiikechukwu24@gmail.com>
2024-01-26 11:51:21 +0100

    Navbar

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
+import { motion } from 'framer-motion'
+import { images } from '../../constants' 
 import './Header.scss'
 
 const Header = () => {
   return (
-    <div>
-      
+    <div className='app__header app__flex'>
+      <motion.div
+       whileInView={{ x: [-100, 0 ], opacity: [ 0, 1 ]}}
+       transition={{ duration: 0.5 }}
+       className='app__header-info'
+      >
+       <div app__header-badge>
+         <div className='badge-cmp app__flex'>
+           <span>👋</span>
+           <div style={{ marginLeft: 20}}>
+            <p className='p-text'>Hello, I am </p>
+            <h1 className='head-text'> Iyke </h1>
+           </div>
+        </div> 
+       </div>
+      </motion.div>
     </div>
   )
 }
